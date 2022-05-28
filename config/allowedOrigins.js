a3072020bc66340bea550951f15902d3c4e83ac1
Ifeanyi <chimaifeanyi29@gmail.com>
2022-05-28 01:04:17 +0100

    update allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,7 +3,8 @@ const allowedOrigins = [
     "http://localhost:3000",
     "http://localhost:5000",
     "http://localhost:3500",
-    'http://127.0.0.1:5500'
+    'http://127.0.0.1:5500',
+    "https://ifeanyi-typescript-student.netlify.app"
 ]
 
 module.exports = allowedOrigins;
